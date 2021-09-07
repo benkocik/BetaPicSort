5de9c1af634226eca9ad0a8537e669ce2d007048
Ben Kocik <bkocik@heestand.tech>
2021-09-07 16:44:48 -0400

    added main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# Programmer: Ben Kocik
+# Description: Automatically sorts pictures into dated folders
+
+# Imports
+import os
+import time
+
+def main():
+    # Mount the google drive folder
+    histPath = os.path.join("mnt", "HistDrive")
+    os.system("sudo rclone mount --allow-other --daemon picsort: " + histPath)
+
+    # Sort through pictures
+    dumpPath = os.path.join(histPath, "ToSort")
+    for pic in os.listdir(dumpPath):
+        picDate = time.ctime(os.path.getmtime(pic))
+        print(picDate)
+        #for f in os.walk(histPath):
+
+
+    # Unmount the google drive folder
+    os.system("fusermount -u " + histPath)
+
+
+
+if __name__ == "__main__":
+    main()
