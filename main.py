4ed98fb9a3624c3be4a7009e708ca1b0e4a14d13
Ben Kocik <bkocik@heestand.tech>
2021-09-08 21:53:02 -0400

    debugging

--- main.py
+++ main.py
@@ -21,12 +21,12 @@ def main():
         print(picYear)
         for f in os.listdir(histPath):
             # Confirm it is a directory
+            print(f)
+            print(f[:3])
+            print(picYear[:3])
             if os.path.isdir(f):
                 # Check decade folder
-                if str(f[:3]) == str(picYear[:3]):
-                    print(f)
-                    print(f[:3])
-                    print(picYear[:3])
+                if f[:3] == picYear[:3]:
                     for j in os.listdir(os.path.join(histPath, f)):
                         print(j)
                         # Confirm it is a directory
